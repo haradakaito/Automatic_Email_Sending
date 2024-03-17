dc5a01defd84c06cd8d647de6d41e64927d44859
--local <--local>
2024-03-17 15:00:06 +0900

    [delete] カレンダー機能の削除

--- master.py
+++ master.py
@@ -10,6 +10,7 @@ def send_daily_report_all():
     db_id_list = GetProperties().get_dbid_list()  # 各ユーザーのデータベースIDを取得
     user_name_list = GetProperties().get_user_name_list(db_id_list)  # 各ユーザーの名前を取得
     sleep_time_list = [random.randint(0, 60*60) for i in range(len(db_id_list))]  # 送信時刻をランダムに設定
+    sleep_time_list = [0 for i in range(len(db_id_list))]
     # LineNotifyで日報送信予定時刻を通知
     notify_list = list(zip(user_name_list, sleep_time_list))
     line_notify_daily_report(notify_list)  
@@ -18,12 +19,14 @@ def send_daily_report_all():
         Thread(target=send_daily_report, args=(db_id, sleep_time)).start()
 
 
-# 20:00に定期実行
-def main():
-    schedule.every(1).day.at('20:00').do(send_daily_report_all)
-    while(True):
-        schedule.run_pending()
-        time.sleep(1)
+# # 20:00に定期実行
+# def main():
+#     schedule.every(1).day.at('20:00').do(send_daily_report_all)
+#     while(True):
+#         schedule.run_pending()
+#         time.sleep(1)
 
+# if __name__ == '__main__':
+#     main()
 if __name__ == '__main__':
-    main()
+    send_daily_report_all()
