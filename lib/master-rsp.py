9f7190ca7b4e5c079f2ea46220844e95bdfcf08a
ShimadaTakutoLuke <smd0223takuto@gmail.com>
2024-03-20 20:26:29 +0900

    [update] ラズパイとWindows用のプログラムの作成

---
+++ lib/master-rsp.py
@@ -0,0 +1,29 @@
+import random
+import time
+from threading import Thread
+from _schedule import Schedule
+from _database_loading import GetProperties
+from _notification import line_notify_daily_report
+from _send_daily_report_mail import send_daily_report
+
+def send_daily_report_all(base_time):
+    if Schedule().is_today_holiday() == False: # 休祝日を除いて送信
+        db_id_list = GetProperties().get_dbid_list()  # 各ユーザーのデータベースIDを取得
+        user_name_list, flag_list = GetProperties().get_user_name_list(db_id_list)  # 各ユーザーの名前を取得
+
+        sleep_time_list = [60*abs(random.gauss(25,20)) for _ in range(len(db_id_list))]  # 送信時刻をランダムに設定
+
+        notify_list = list(zip(user_name_list, sleep_time_list))
+        line_notify_daily_report(notify_list, flag_list, base_time) # LineNotifyで日報送信予定時刻を通知
+
+        # 並列処理で日報送信
+        thread_list = []
+        for db_id, sleep_time in zip(db_id_list, sleep_time_list):
+            thread = Thread(target=send_daily_report, args=(db_id, sleep_time))
+            thread.start()
+            thread_list.append(thread)
+        for thread in thread_list:
+            thread.join()
+
+if __name__ == '__main__':
+    send_daily_report_all(base_time=['19','45'])
