2f3c465d1140ac0bee9a100d3dbd8f7a4b78278c
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-03-29 00:10:42 +0900

    [update]NotionDBに不要行があった場合でも停止しないように修正しました

--- lib/_database_loading.py
+++ lib/_database_loading.py
@@ -8,8 +8,6 @@ class GetProperties:
     conf_path = current_dir / '../config/config.json'
     conf = json.load(open(conf_path, 'r', encoding='utf-8'))
 
-    NOTION_MASTER_ID = conf['database_loading']['NOTION_MASTER_ID']
-
     # 全登録ユーザーのデータベースIDリストを取得
     def get_dbid_list(self):
         NOTION_ACCESS_TOKEN = self.conf['master']['NOTION_ACCESS_TOKEN']
@@ -18,10 +16,14 @@ class GetProperties:
         self.r = client.databases.query(NOTION_MASTER_ID)
 
         # パース部
-        self.dbid_list = []
-        for i in range(len(self.r['results'])):
-            self.id = self.r['results'][i]['properties']['データベースID']['title'][0]['plain_text']
-            self.dbid_list.append(self.id)
+        try:
+            self.dbid_list = []
+            for i in range(len(self.r['results'])):
+                self.id = self.r['results'][i]['properties']['データベースID']['title'][0]['plain_text']
+                self.dbid_list.append(self.id)
+        except:
+            error_time = dt.datetime.now().strftime('[%Y/%m/%d] %H:%M')
+            print(f'{error_time} NOTION_ERROR_001')
         return self.dbid_list
     
     # ユーザー名リストを取得
@@ -41,35 +43,31 @@ class GetProperties:
         self.r = self.client.databases.query(NOTION_DATABASE_ID)
 
         # パース部
-        self.mailinfo_dict, self.flag = self._get_personal_info(self.r)
+        try:
+            self.mailinfo_dict, self.flag = self._get_personal_info(self.r)
+        except:
+            error_time = dt.datetime.now().strftime('[%Y/%m/%d] %H:%M')
+            print(f'{error_time} NOTION_ERROR_002')
         return self.mailinfo_dict, self.flag
     
     # db_infoのjsonパーサー
     def _get_personal_info(self, db_json:str):
-
-        self.flag = True
-        self.name = db_json['results'][0]['properties']['苗字']['title'][0]['plain_text']
-
+        self.name = db_json['results'][len(db_json['results'])-1]['properties']['苗字']['title'][0]['plain_text']
         property_list = ['学年', '静大メール', 'パスワード', '進捗項目', '進捗マップ', '署名', '自由記入欄']
-        if not any([len(db_json['results'][0]['properties'][p]['rich_text'])==0 for p in property_list]): # 全プロパティが空でない
-            self.grade = db_json['results'][0]['properties']['学年']['rich_text'][0]['plain_text']
-            self.email = db_json['results'][0]['properties']['静大メール']['rich_text'][0]['plain_text']
-            self.password = db_json['results'][0]['properties']['パスワード']['rich_text'][0]['plain_text']
-            self.progress = db_json['results'][0]['properties']['進捗項目']['rich_text'][0]['plain_text']
-            self.progress_map = db_json['results'][0]['properties']['進捗マップ']['rich_text'][0]['plain_text']
-            self.signature = db_json['results'][0]['properties']['署名']['rich_text'][0]['plain_text']
-            self.other = db_json['results'][0]['properties']['自由記入欄']['rich_text'][0]['plain_text']
+        if not any([len(db_json['results'][len(db_json['results'])-1]['properties'][p]['rich_text'])==0 for p in property_list]): # 全プロパティが空でない
+            self.grade = db_json['results'][len(db_json['results'])-1]['properties']['学年']['rich_text'][0]['plain_text']
+            self.email = db_json['results'][len(db_json['results'])-1]['properties']['静大メール']['rich_text'][0]['plain_text']
+            self.password = db_json['results'][len(db_json['results'])-1]['properties']['パスワード']['rich_text'][0]['plain_text']
+            self.progress = db_json['results'][len(db_json['results'])-1]['properties']['進捗項目']['rich_text'][0]['plain_text']
+            self.progress_map = db_json['results'][len(db_json['results'])-1]['properties']['進捗マップ']['rich_text'][0]['plain_text']
+            self.signature = db_json['results'][len(db_json['results'])-1]['properties']['署名']['rich_text'][0]['plain_text']
+            self.other = db_json['results'][len(db_json['results'])-1]['properties']['自由記入欄']['rich_text'][0]['plain_text']
+            self.mailinfo_dict = {'name':self.name, 'grade':self.grade, 'email':self.email, 'password':self.password, 'progress':self.progress, 'progress_map':self.progress_map, 'signature':self.signature, 'other':self.other}
+            self.flag = True
         else:
-            self.grade = ''
-            self.email = ''
-            self.password = ''
-            self.progress = ''
-            self.progress_map = ''
-            self.signature = ''
-            self.other = ''
+            self.mailinfo_dict = {'name':'', 'grade':'', 'email':'', 'password':'', 'progress':'', 'progress_map':'', 'signature':'', 'other':''}
             self.flag = False
-
-        self.mailinfo_dict = {'name':self.name, 'grade':self.grade, 'email':self.email, 'password':self.password, 'progress':self.progress, 'progress_map':self.progress_map, 'signature':self.signature, 'other':self.other}
+                
         return self.mailinfo_dict, self.flag
     
     # 指定したプロパティを更新
@@ -81,5 +79,4 @@ class GetProperties:
             client.pages.update(page_id=r['results'][0]['id'], properties={property_name: {'rich_text': [{'type':'text', 'text':{'content':{new_contents}}}]}})
         except:
             error_time = dt.datetime.now().strftime('[%Y/%m/%d] %H:%M')
-            print(f'{error_time} NOTION_ERROR')
-        
+            print(f'{error_time} NOTION_ERROR_003')
