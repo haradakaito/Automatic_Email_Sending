10784dd857bd0dd45563d5f0b9ae020bb9c9b2ed
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-04-24 12:53:40 +0900

    [clean] リファクタリング

--- lib/_send_daily_report_mail.py
+++ lib/_send_daily_report_mail.py
@@ -11,15 +11,35 @@ conf_path = current_dir / '../config/config.json'
 conf = json.load(open(conf_path, 'r', encoding='utf-8'))
 
 def send_daily_report(db_id, sleep_time):
+    """
+        メールで日報を送信する
+        
+        Parameters
+        ---
+        db_id : str
+            データベースID
+        sleep_time : int
+            送信時刻までの待機時間（秒）
+        
+        Returns
+        ---
+        None
+    """
 
-    time.sleep(sleep_time) # sleep_time秒待機
-    user_info, _ = GetProperties().get_db_info(db_id=db_id) # db_idからメールを送信
-    if not any([p=='' for p in user_info.values()]): # user_infoが全て記入されている
+    # sleep_time秒待機
+    time.sleep(sleep_time)
+    # db_idからメールを送信するユーザー情報を取得
+    user_info, _ = GetProperties().get_db_info(db_id=db_id)
+    # user_infoが全て記入されている場合のみメール送信
+    if not any([p=='' for p in user_info.values()]):
+        # メール内容の作成
         mail_contents = Contents(**user_info)
-        subject = mail_contents.create_subject() # 件名
-        body = mail_contents.create_body() # 本文
+        # 件名と本文の作成
+        subject = mail_contents.create_subject()
+        # 本文の作成
+        body = mail_contents.create_body()
+        # 送信先メールアドレス
         to_email = conf['send_daily_report_mail']['to_email']
-
-        # 送信
+        # メール送信
         sender = Sender(from_email=user_info['email'], to_email=to_email, user_name=user_info['email'], password=user_info['password'], subject=subject, body=body)
         sender.send()
