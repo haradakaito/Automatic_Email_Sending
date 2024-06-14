6204ff6104ea6184eff51f1083f18f813489cbfc
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-06-14 18:34:09 +0900

    [add] ユーザーごとにエラー内容を通知するように変更

--- lib/main-rsp.py
+++ lib/main-rsp.py
@@ -18,7 +18,7 @@ googlecalendartools = GoogleCalendarTools()
 contents            = Contents()
 
 # 送信結果を保存する辞書
-send_results = {}
+results = {} # {name: result_message}
 
 # メイン処理
 def main() -> None:
@@ -31,11 +31,8 @@ def main() -> None:
     threads = []
     all_user_wait = {}
     for dbid, result in checked_dbid.items():
-        # 送信可能な場合
         if result:
-            # 待機時間を取得
-            wait_second         = utils.setting_wait_second()
-            # ユーザー名と待機時間を辞書に保存
+            wait_second         = utils.setting_wait_second() # 待機時間を取得
             name                = notiontools.get_property(dbid, "苗字")
             all_user_wait[name] = wait_second
             # スレッドで実行（入力：データベースID，待機時間）
@@ -49,52 +46,61 @@ def main() -> None:
     for thread in threads:
         thread.join()
     # 送信結果を通知
-    if all([result == True for result in send_results.values()]): # すべての送信が成功した場合
+    if all([result == True for result in results.values()]): # すべての送信が成功した場合
         pass
     else:
-        linenotifytools.notify_send_result(send_results=send_results)
+        linenotifytools.notify_send_result(send_results=results)
 
 # 事前通知
 def _pre_notify(wait_second) -> None:
     checked_user     = notiontools.check_all_user()
     correctable_time = datetime.now() + timedelta(seconds=wait_second)
     linenotifytools.notify_checked_user(checked_user=checked_user, correctable_time=correctable_time)
-    # time.sleep(wait_second)
+    time.sleep(wait_second)
 
 # ユーザーごとの処理
 def _user_process(dbid:str, wait_second:int) -> None:
     # ユーザー情報を取得
-    name         = notiontools.get_property(dbid, "苗字")
-    grade        = notiontools.get_property(dbid, "学年")
-    from_addr    = notiontools.get_property(dbid, "静大メール")
-    password     = notiontools.get_property(dbid, "パスワード")
-    progress     = notiontools.get_property(dbid, "進捗項目")
-    progress_map = notiontools.get_property(dbid, "進捗マップ")
-    signature    = notiontools.get_property(dbid, "署名")
-    free         = notiontools.get_property(dbid, "自由記入欄")
-    event        = googlecalendartools.get_event(name=name)
-    
+    try:
+        name         = notiontools.get_property(dbid, "苗字")
+        grade        = notiontools.get_property(dbid, "学年")
+        from_addr    = notiontools.get_property(dbid, "静大メール")
+        password     = notiontools.get_property(dbid, "パスワード")
+        progress     = notiontools.get_property(dbid, "進捗項目")
+        progress_map = notiontools.get_property(dbid, "進捗マップ")
+        signature    = notiontools.get_property(dbid, "署名")
+        free         = notiontools.get_property(dbid, "自由記入欄")
+        event        = googlecalendartools.get_event(name=name)
+    except:
+        results[name] = "DBの取得に失敗"
+        return
     # 件名と本文を作成
-    subject      = contents.create_subject()
-    body         = contents.create_body(
-        name         = name,
-        grade        = grade,
-        progress     = progress,
-        progress_map = progress_map,
-        event        = event,
-        signature    = signature,
-        free         = free
-        )
+    try:
+        subject      = contents.create_subject()
+        body         = contents.create_body(
+            name         = name,
+            grade        = grade,
+            progress     = progress,
+            progress_map = progress_map,
+            event        = event,
+            signature    = signature,
+            free         = free
+            )
+    except:
+         results[name] = "メールの作成に失敗"
+         return
     # メール送信（wait_second秒だけ待機して送信）
-    # time.sleep(wait_second)
-    send_result = mailsender.send_mail(
-        from_addr = from_addr,
-        subject   = subject,
-        body      = body,
-        password  = password
-        )
-    # 送信結果を保存
-    send_results[name] = send_result
+    time.sleep(wait_second)
+    try:
+        send_result = mailsender.send_mail(
+            from_addr = from_addr,
+            subject   = subject,
+            body      = body,
+            password  = password
+            )
+    except:
+        results[name] = "メールの送信に失敗"
+        return
 
 if __name__ == "__main__":
     if utils.today_is_holiday():
